67bfac14a365bb03323576add35e579f803a9b30
hasan <hasanwatheq2014@gmail.com>
2023-04-02 18:30:58 -0400

    Moved filterCount.java and filterLength.java into main as methods

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -2,9 +2,158 @@ package com.mycompany.fileprocesser;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class FileProcesser {
+public class FileProcesser 
+{
+    
+    public static ArrayList<File> newFile = new ArrayList<File>();
 
+    public static void printList()
+    {
+        String newS = newFile.toString();
+        
+        System.out.println(newS);
+        
+    }
+    
+    public static void filterLength(File[] file, long Length, String Operator)
+    {
+        switch(Operator)
+        {
+            case "EQ":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() == Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+                
+            case "NEQ":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() != Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+                
+            case "GT":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() > Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+            case "GTE":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() >= Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+            case "LT":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() < Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+            case "LTE":
+                if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        if(child.length() <= Length)
+                        {
+                            newFile.add(child);
+                        }
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+                break;
+        }
+    }
+    
+    public static void filterCount(File[] file, String Key, int value) throws FileNotFoundException
+    {
+        int keyCounter = 0;
+        if (file != null)
+                {
+                    for (File child : file)
+                    {
+                        String[] words = null;
+                        Scanner inp = new Scanner(child);
+                        String splitter;
+                            while(inp.hasNext())
+                            {
+                                splitter = inp.nextLine();
+                                words = splitter.split(" ");
+                                for (String word : words)
+                                {
+                                    if(word.equals(Key))
+                                        keyCounter++;
+                                }
+                            }
+                            if(keyCounter >= value)
+                                newFile.add(child);
+                            keyCounter = 0;
+                    }
+                }
+                else
+                {
+                    System.out.println("An error has occured!");
+                }
+    }
+    
     public static void main(String[] args) throws FileNotFoundException, IOException 
     {
         File file = new File("C:\\Users\\hasan\\OneDrive\\Desktop\\Programming2\\Java\\Processing Files");
@@ -17,15 +166,13 @@ public class FileProcesser {
         {
             dirList[0] = file;
         }
-        filterLength fL = new filterLength(dirList ,0 , "NEQ");
-        fL.lengther();
+        filterLength(dirList ,20000 , "GTE");
         System.out.println("List of length: ");
-        fL.printList();
+        printList();
         
-        filterCount fl = new filterCount(dirList, "hasana", 1);
-        fl.counter();
+        filterCount(dirList, "hasana", 1);
         System.out.println("List of count: ");
-        fl.printList();
+        printList();
         
     }
 }
