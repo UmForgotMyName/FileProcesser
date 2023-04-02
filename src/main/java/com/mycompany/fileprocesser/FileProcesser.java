68497e47ed35043cddfedf76fc6702a6af80bb2e
hasan <hasanwatheq2014@gmail.com>
2023-04-02 16:38:52 -0400

    Created the classes for the filters

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.fileprocesser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-/**
- *
- * @author tylerwarwick
- */
 public class FileProcesser {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Test!");
+    public static void main(String[] args) throws FileNotFoundException, IOException 
+    {
+        File file = new File("C:\\Users\\hasan\\OneDrive\\Desktop\\Programming2\\Java\\Processing Files");
+        File[] dirList = {file};
+        if(file.isDirectory())
+        {
+            dirList = file.listFiles();
+        }
+        else
+        {
+            dirList[0] = file;
+        }
+        filterLength fL = new filterLength(dirList ,0 , "NEQ");
+        fL.lengther();
+        System.out.println("List of length: ");
+        fL.printList();
+        
+        filterCount fl = new filterCount(dirList, "hasana", 1);
+        fl.counter();
+        System.out.println("List of count: ");
+        fl.printList();
+        
     }
 }
