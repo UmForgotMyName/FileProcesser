4d33dd24e01fe7c044a80c5065d73d5161d1d21a
UmForgotMyName <rehan1234444@gmail.com>
2023-04-02 17:08:13 -0400

    Filter by name and filter by content

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -4,13 +4,58 @@
 
 package com.mycompany.fileprocesser;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 /**
  *
- * @author tylerwarwick
+ * @author tylerwarwick, rehansiddiqi
  */
 public class FileProcesser {
 
     public static void main(String[] args) {
         System.out.println("Hello Test!");
+    }
+    
+    public List<File> filterName(List<File> entries, String key) {
+        for (int i = 0; i < entries.size(); i++) {
+            if (!entries.get(i).getName().equals(key)) {
+                //entries.get(i).delete(); not sure if this is needed
+                entries.remove(i);
+            }
+        }
+        return entries;
+    }
+
+    public List<File> filterContent(List<File> entries, String key) {
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).isFile()) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(entries.get(i)))) {
+                    String line;
+                    boolean flag = false;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains(key)) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    
+                    if (!flag) {
+                        //entries.get(i).delete(); not sure if this is needed
+                        entries.remove(i);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                //entries.get(i).delete(); not sure if this is needed
+                entries.remove(i);
+            }
+        }
+        return entries;
     }
 }
