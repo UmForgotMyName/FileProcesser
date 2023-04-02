62f3bf3f91e5fb92b0a755dff3b310c25e263f09
BigboiUzi <syeduzai@uoguelph.ca>
2023-04-02 18:47:16 -0400

    Updated Rename and List Methods

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -18,44 +18,54 @@ public class FileProcesser {
         System.out.println("Hello Test!");
     }
     
-    public static List<File> RenameMethod( List<File> entries, String suffix){
-        
-        List<File> renamedEntries = new ArrayList<File>();
-       
-        for (File file : entries) {
-            String fileName = file.getName();
-            String[] parts = fileName.split("\\.");
-            String baseName = parts[0];
-            String extension = "." + parts[1];
-            String newName = baseName + suffix + extension;
-            File newFile = new File(file.getParent(), newName);
-            
-            if (file.renameTo(newFile)) {
-                System.out.println(newName);
-                renamedEntries.add(newFile);
-            } else {
-                System.out.println("Failed to rename file: " + file.getName());
-                renamedEntries.add(newFile);
+     public static List<File> ListMethod(List<File> entries, int max) {
+        List<File> result = new ArrayList<>();
+        for (int i = 0; i < entries.size(); i++) {
+            if (max == 0) {
+                break;
+            }
+            if (entries.get(i).isFile()) {
+                result.add(entries.get(i));
+                max--;
+            } else if (entries.get(i).isDirectory()) {
+                File[] temp = entries.get(i).listFiles();
+                for (int j = 0; j < temp.length; j++) {
+                    if (max == 0) {
+                        break;
+                    }
+                    result.add(temp[j]);
+                    max--;
+                }
             }
         }
-        
-        entries = renamedEntries;
-        return entries;
+        return result;
     }
-    
-    public static List<File> ListMethod (List<File> entries, int MAX){
-         if (entries.size() > MAX) {
-            entries = entries.subList(0, MAX); // reduce ArrayList if too large
-        } 
-        
-        System.out.println("Reduced ArrayList:");
-        for (File file : entries) {
-            System.out.println(file.getName());
-            file.renameTo(new File("C:\\Users\\Zebaa\\OneDrive - University of Guelph\\newDummy\\" + file.getName()));
-            
+     
+    public static List<File> RenameMethod(List<File> entries, String suffix) {
+        List<File> result = new ArrayList<>();
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).isFile()) {
+                String fileName = entries.get(i).getName();
+                String[] parts = fileName.split("\\.");
+                String baseName = parts[0];
+                String extension = "." + parts[1];
+                String newName = baseName + suffix + extension;
+                File newFile = new File(entries.get(i).getParent(), newName);
+                result.add(newFile);
+            } else if (entries.get(i).isDirectory()) {
+                File[] temp = entries.get(i).listFiles();
+                for (int j = 0; j < temp.length; j++) {
+                    String fileName = temp[j].getName();
+                    String[] parts = fileName.split("\\.");
+                    String baseName = parts[0];
+                    String extension = "." + parts[1];
+                    String newName = baseName + suffix + extension;
+                    File newFile = new File(temp[j].getParent(), newName);
+                    result.add(newFile);
+                }
+            }
         }
-        
-        System.out.println(entries);
-        return entries;
-    }
+        return result;
+    }    
+    
 }
