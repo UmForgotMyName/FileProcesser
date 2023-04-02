f82307d807d8094e55c25b05833f042c886d8096
BigboiUzi <syeduzai@uoguelph.ca>
2023-04-02 17:23:12 -0400

    Rename and List Methods

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.fileprocesser;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author tylerwarwick
@@ -12,5 +16,46 @@ public class FileProcesser {
 
     public static void main(String[] args) {
         System.out.println("Hello Test!");
+    }
+    
+    public static List<File> RenameMethod( List<File> entries, String suffix){
+        
+        List<File> renamedEntries = new ArrayList<File>();
+       
+        for (File file : entries) {
+            String fileName = file.getName();
+            String[] parts = fileName.split("\\.");
+            String baseName = parts[0];
+            String extension = "." + parts[1];
+            String newName = baseName + suffix + extension;
+            File newFile = new File(file.getParent(), newName);
+            
+            if (file.renameTo(newFile)) {
+                System.out.println(newName);
+                renamedEntries.add(newFile);
+            } else {
+                System.out.println("Failed to rename file: " + file.getName());
+                renamedEntries.add(newFile);
+            }
+        }
+        
+        entries = renamedEntries;
+        return entries;
+    }
+    
+    public static List<File> ListMethod (List<File> entries, int MAX){
+         if (entries.size() > MAX) {
+            entries = entries.subList(0, MAX); // reduce ArrayList if too large
+        } 
+        
+        System.out.println("Reduced ArrayList:");
+        for (File file : entries) {
+            System.out.println(file.getName());
+            file.renameTo(new File("C:\\Users\\Zebaa\\OneDrive - University of Guelph\\newDummy\\" + file.getName()));
+            
+        }
+        
+        System.out.println(entries);
+        return entries;
     }
 }
