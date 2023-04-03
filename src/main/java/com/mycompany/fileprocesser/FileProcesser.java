d13dbd85663e28e59df41d9866232aa92f658cb3
BigboiUzi <syeduzai@uoguelph.ca>
2023-04-03 14:57:07 -0400

    Commented Rename and List Method

--- src/main/java/com/mycompany/fileprocesser/FileProcesser.java
+++ src/main/java/com/mycompany/fileprocesser/FileProcesser.java
@@ -16,18 +16,22 @@ import java.util.List;
 public class FileProcesser {
 
     public static void main(String[] args) {
-        System.out.println("Hello Test!");
+        
     }
-    
+    //List method sorts the arraylist in regards to the max number
      public static List<File> ListMethod(List<File> entries, int max) throws IOException {
-        List<File> result = new ArrayList<>();
+         // Parameter consists of the the arraylist and the max value
+        List<File> result = new ArrayList<>();//Result arraylist which stores updated arraylist
         for (int i = 0; i < entries.size(); i++) {
+            //statement if max value is 0
             if (max == 0) {
                 break;
             }
+            //statement if entry is a set of files
             if (entries.get(i).isFile()) {
                 result.add(entries.get(i));
                 max--;
+                //statement if entry is a directory 
             } else if (entries.get(i).isDirectory()) {
                 File[] temp = entries.get(i).listFiles();
                 for (int j = 0; j < temp.length; j++) {
@@ -39,22 +43,27 @@ public class FileProcesser {
                 }
             }
         }
+        //Program returns updated arraylist with set changes
         return result;
     }
-     
+    
+    //Rename method which adds a suffix to the file name 
     public static List<File> RenameMethod(List<File> entries, String suffix) throws IOException {
-        List<File> result = new ArrayList<>();
+        List<File> result = new ArrayList<>();//Result arraylist which stores updated arraylist
+        //if entry is a file, rename method applies suffix to each individual file
         for (int i = 0; i < entries.size(); i++) {
             if (entries.get(i).isFile()) {
                 String fileName = entries.get(i).getName();
-                String[] parts = fileName.split("\\.");
+                String[] parts = fileName.split("\\.");//Regular expression which looks for "." in file name
                 String baseName = parts[0];
                 String extension = "." + parts[1];
-                String newName = baseName + suffix + extension;
+                String newName = baseName + suffix + extension;//newname string holds updated name
                 File newFile = new File(entries.get(i).getParent(), newName);
-                result.add(newFile);
+                result.add(newFile);//Add updated arraylist to result array
+                //If entry is a directory, program searches in directory and lists individual files
             } else if (entries.get(i).isDirectory()) {
-                File[] temp = entries.get(i).listFiles();
+                File[] temp = entries.get(i).listFiles();//temp storage for files in directory
+                //Rename methods applied to temp array which holds files 
                 for (int j = 0; j < temp.length; j++) {
                     String fileName = temp[j].getName();
                     String[] parts = fileName.split("\\.");
@@ -62,10 +71,11 @@ public class FileProcesser {
                     String extension = "." + parts[1];
                     String newName = baseName + suffix + extension;
                     File newFile = new File(temp[j].getParent(), newName);
-                    result.add(newFile);
+                    result.add(newFile);//Result arraylist receives updated names 
                 }
             }
         }
+        //program returns updated arraylist with set changes 
         return result;
     }    
     
